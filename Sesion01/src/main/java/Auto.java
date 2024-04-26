0d43ab275c8d88ddfb7d9ca9193e567ce101644c
EilleenCamarena <166665541+EilleenCamarena@users.noreply.github.com>
2024-04-26 16:09:21 -0600

    Delete Sesion01 directory

--- Sesion01/src/main/java/Auto.java
+++
@@ -1,9 +0,0 @@
-public class Auto {//es el esquelo que tiene que tener un objeto auto y las propiedades
-    String color;
-    String marca;
-
-    public void start(){
-        System.out.println("Run :3");
-    }
-
-}
--- Sesion01/src/main/java/HolaMundo.java
+++
@@ -1,5 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) { //args coleccion de argumentos al ejecutarse
-        System.out.println("Holi Mundo!!");
-    }
-}
